dd3ea6e4804afe96c10d8a78d82895144220ee55
Afonso Dutra Nogueira Filho <mzt.adnfilho@voegol.com.br>
2019-08-02 13:49:26 -0300

    Fix Version

---
+++ LoggerConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggerConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var log = new Afonsoft.Logger.AfonsoftLoggerProvider<Program>(c=> c.Extension="aft").CreateLogger();
+            log.LogInformation("LogInformation");
+            log.LogError("LogError");
+            log.LogDebug("LogDebug");
+            log.LogWarning("LogWarning");
+            log.LogTrace("LogTrace");
+            log.LogCritical("LogCritical");
+            Console.ReadKey();
+        }
+    }
+}
